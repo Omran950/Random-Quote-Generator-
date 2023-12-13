a6fc6ab54aed635156728bb6db4c06b2984f20d0
Omran950 <mohammedomran32@gmail.com>
2023-12-13 22:36:47 +0200

    first project

---
+++ js/main.js
@@ -0,0 +1,190 @@
+var quotes = [
+  {
+    quote: "Be yourself; everyone else is already taken",
+    author: "Oscar Wilde",
+    photo:
+      '<img src="images/Oscar-Wilde-Portrait.jpg" alt="Oscar Wilde" class="d-block w-100" />',
+  },
+  {
+    quote: "The only thing we have to fear is fear itself",
+    author: "Franklin D. Roosevelt",
+    photo:
+      '<img src="images/franklin_d_roosevelt.jpg" alt="Franklin D. Roosevelt" class="d-block w-100"/>',
+  },
+  {
+    quote:
+      "The only limit to our realization of tomorrow will be our doubts of today. Let us move forward with strong and active faith",
+    author: "Franklin D. Roosevelt",
+    photo:
+      '<img src="images/franklin_d_roosevelt.jpg" alt="Franklin D. Roosevelt" class="d-block w-100"/>',
+  },
+  {
+    quote:
+      "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe",
+    author: "Albert Einstein",
+    photo:
+      '<img src="images/images.jpg" alt="Einstein" class="d-block w-100" />',
+  },
+  {
+    quote: "A person who has never made a mistake has never tried anything new",
+    author: "Albert Einstein",
+    photo:
+      '<img src="images/images.jpg" alt="Einstein" class="d-block w-100" />',
+  },
+  {
+    quote: "Creativity is intelligence having fun",
+    author: "Albert Einstein",
+    photo:
+      '<img src="images/images.jpg" alt="Einstein" class="d-block w-100" />',
+  },
+  {
+    quote:
+      "Logic will get you from A to B; imagination will take you everywhere",
+    author: "Albert Einstein",
+    photo:
+      '<img src="images/images.jpg" alt="Einstein" class="d-block w-100" />',
+  },
+  {
+    quote: "A room without books is like a body without a soul",
+    author: "Marcus Tullius Cicero",
+    photo:
+      '<img src="images/Marcus Tullius Cicero.jpg" alt="Marcus Tullius Cicero" class="d-block w-100" />',
+  },
+  {
+    quote: "Any man can make mistakes, but only an idiot persists in his error",
+    author: "Marcus Tullius Cicero",
+    photo:
+      '<img src="images/Marcus Tullius Cicero.jpg" alt="Marcus Tullius Cicero" class="d-block w-100" />',
+  },
+  {
+    quote: "If you have a garden and a library, you have everything you need",
+    author: "Marcus Tullius Cicero",
+    photo:
+      '<img src="images/Marcus Tullius Cicero.jpg" alt="Marcus Tullius Cicero" class="d-block w-100" />',
+  },
+  {
+    quote:
+      "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind",
+    author: "Bernard M. Baruch",
+    photo:
+      '<img src="images/Bernard M. Baruch.jpg" alt="Bernard M. Baruch" class="d-block w-100" />',
+  },
+  {
+    quote:
+      "You've gotta dance like there's nobody watching,Love like you'll never be hurt,Sing like there's nobody listening,And live like it's heaven on earth",
+    author: "William W. Purkey",
+    photo:
+      '<img src="images/William W. Purkey0,200_.jpg" alt="William W. Purkey" class="d-block w-100" />',
+  },
+  {
+    quote: "If you tell the truth, you don't have to remember anything",
+    author: "Mark Twain",
+    photo:
+      '<img src="images/Mark-Twain-Portrait.webp" alt="Mark Twain" class="d-block w-100" />',
+  },
+  {
+    quote:
+      "Live as if you were to die tomorrow. Learn as if you were to live forever",
+    author: "Mahatma Gandhi",
+    photo:
+      ' <img src="images/mahatma Gandhiginal.jpg" alt="Mahatma Gandhi" class="d-block w-100" />',
+  },
+  {
+    quote: "Without music, life would be a mistake",
+    author: "Friedrich Nietzsche",
+    photo:
+      '<img src="images/friedrich-nietzsche-painting-paul-meijering.jpg" alt="Friedrich Nietzsche" class="d-block w-100" />',
+  },
+  {
+    quote: "Life is really simple, but we insist on making it complicated",
+    author: "Friedrich Nietzsche",
+    photo:
+      '<img src="images/friedrich-nietzsche-painting-paul-meijering.jpg" alt="Friedrich Nietzsche" class="d-block w-100" />',
+  },
+  {
+    quote:
+      "Our greatest glory is not in never falling, but in rising every time we fall",
+    author: "Friedrich Nietzsche",
+    photo:
+      '<img src="images/friedrich-nietzsche-painting-paul-meijering.jpg" alt="Friedrich Nietzsche" class="d-block w-100" />',
+  },
+  {
+    quote:
+      "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment",
+    author: "Ralph Waldo Emerson",
+    photo:
+      '<img src="images/Ralph-Waldo-Emerson-Portrait.webp" alt="Ralph Waldo Emerson" class="d-block w-100" />',
+  },
+  {
+    quote:
+      "It is better to be hated for what you are than to be loved for what you are not",
+    author: "Andre Gide",
+    photo:
+      '<img src="images/Andre Gide.jpg" alt="Andre Gide" class="d-block w-100" />',
+  },
+  {
+    quote:
+      "Success is not final, failure is not fatal: It is the courage to continue that counts",
+    author: "Winston Churchill",
+    photo:
+      '<img src="images/Winston_Churchil_in_Siren_Suit-503x658.jpg" alt="Winston Churchill" class="d-block w-100" />',
+  },
+  {
+    quote:
+      "Courage is what it takes to stand up and speak; courage is also what it takes to sit down and listen",
+    author: "Winston Churchill",
+    photo:
+      '<img src="images/Winston_Churchil_in_Siren_Suit-503x658.jpg" alt="Winston Churchill" class="d-block w-100" />',
+  },
+  {
+    quote:
+      "We make a living by what we get, but we make a life by what we give",
+    author: "Winston Churchill",
+    photo:
+      '<img src="images/Winston_Churchil_in_Siren_Suit-503x658.jpg" alt="Winston Churchill" class="d-block w-100" />',
+  },
+  {
+    quote: "The purpose of our lives is to be happy",
+    author: "Dalai Lama",
+    photo:
+      '<img src="images/dalai-lama-gettyimages-478851138.jpg" class="d-block w-100" alt="Dalai Lama" />',
+  },
+  {
+    quote:
+      "Everyone can rise above their circumstances and achieve success if they are dedicated to and passionate about what they do",
+    author: "Nelson Mandela",
+    photo:
+      '<img src="images/Nelson Mandelaetty-images.jpg" class="d-block w-100" alt="Nelson Mandela" />',
+  },
+  {
+    quote:
+      "The greatest glory in living lies not in never falling, but in rising every time we fall",
+    author: "Nelson Mandela",
+    photo:
+      '<img src="images/Nelson Mandelaetty-images.jpg" class="d-block w-100" alt="Nelson Mandela" />',
+  },
+  {
+    quote:
+      "I learned that courage was not the absence of fear, but the triumph over it. The brave man is not he who does not feel afraid, but he who conquers that fear",
+    author: "Nelson Mandela",
+    photo:
+      '<img src="images/Nelson Mandelaetty-images.jpg" class="d-block w-100" alt="Nelson Mandela" />',
+  },
+];
+
+console.log(quotes.length);
+
+var pQuote = document.getElementById("quote");
+var pAuthor = document.getElementById("author");
+var photo = document.getElementById("photo");
+
+function quoteApp() {
+  var randNumber = Math.trunc(Math.random() * quotes.length);
+  while (pQuote.innerHTML === quotes[randNumber].quote) {
+    randNumber = Math.trunc(Math.random() * quotes.length);
+  }
+  console.log(randNumber);
+  pQuote.innerHTML = quotes[randNumber].quote;
+  pAuthor.innerHTML = quotes[randNumber].author;
+  photo.innerHTML = quotes[randNumber].photo;
+}
